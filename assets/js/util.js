c10e479813bde8a9798a3530aca5ef9f302f4e8f
Scott Wlaschin <swlaschin@gmail.com>
2017-07-08 00:05:12 +0100

    Added FontAwesome

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,3 +1,15 @@
 function resizeIframe(obj) {
     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 }
+
+$( document ).ready(function() {
+  
+ 	// Add anchors to headings that have ids
+    // https://github.com/circleci/circleci-docs/pull/97/files
+ 	$("h2, h3, h4, h5, h6").filter("[id]").each(function () {
+ 		$(this).append('<a href="#' + $(this).attr("id") + '"><i class="fa fa-link" style="display: none"></i></a>');
+ 	});
+ 	$("h2, h3, h4, h5, h6").filter("[id]").hover(function () {
+ 		$(this).find("i").toggle();
+ 	});
+  });
