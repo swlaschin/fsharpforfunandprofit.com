15b0a0d2b503aa297f4f5a789ebff00ba53e75b5
swlaschin <swlaschin@gmail.com>
2025-12-29 19:15:09 +0000

    remove rsync

--- _rsync_fsffap.bat
+++
@@ -1,3 +0,0 @@
-rsync -e \tools\rsync\ssh --chmod=D755,F644 --delete -av ./_site/ iniab@fsharpforfunandprofit.com:~/fsharpforfunandprofit.com/
-REM --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r
-pause
--- _rsync_fsffap.sh
+++
@@ -1,2 +0,0 @@
-rsync -e ssh --chmod=D755,F644 --delete -av ./public/ iniab@fsharpforfunandprofit.com:~/fsharpforfunandprofit.com/
-
